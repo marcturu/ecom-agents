819c8416bcfbc808998392eee723f16fc419f6a0
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-05-24 17:27:05 +0200

    update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
-from app import app
+"""
+Routes for the Flask application.
 
-@app.route('/')
-@app.route('/index')
+This module contains the routes for the Flask application.
+
+"""
+
+from flask import Blueprint
+
+# Create a blueprint object
+routes = Blueprint('routes', __name__)
+
+
+@routes.route('/')
+@routes.route('/index')
 def index():
+    """
+    A route for the home page of the application.
+
+    This route returns a simple "Hello, World!" message.
+
+    Returns:
+        str: The message "Hello, World!".
+
+    """
     return "Hello, World!"
