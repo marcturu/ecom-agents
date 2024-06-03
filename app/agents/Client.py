c19e4358e75ed75106e31294a535c2a15ef98cc4
Sergi Campuzano <campuzano.sergi@gmail.com>
2024-06-03 21:13:28 +0200

    usuari registrat

--- app/agents/Client.py
+++ app/agents/Client.py
@@ -23,15 +23,87 @@ RECOMANADOR_URL = 'http://localhost:5010'
 AGENTE_VALORACIONES_URL = 'http://localhost:5005'
 
 
-USER_ID = "Manolo"
-
 # Definir el namespace
 ns = Namespace(
     "http://www.semanticweb.org/hp/ontologies/2024/4/PracticaECSDI#")
 
+archivo_base_datos_usuarios = "../data/usuaris.rdf"
+
+USUARIO_REGISTRADO = {}
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        direccion = request.form['direccion']
+
+        # Crear un JSON con los datos del usuario
+        USUARIO_REGISTRADO['nombre'] = nombre
+        USUARIO_REGISTRADO['correo'] = correo
+        USUARIO_REGISTRADO['direccion'] = direccion
+
+        registrarUsuarioBD(nombre, correo, direccion)
+
+        return """
+            <h1>¡Cliente registrado correctamente!</h1>
+            <form action="/buscar" method="get">
+                <button type="submit">Anar pagina principal</button>
+            </form>
+            """
+    else: 
+        html = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Filtrar Productes</title>
+            </head>
+            <body>
+                <h1>Registro de Cliente</h1>
+                <form method="post">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required><br><br>
+
+                    <label for="correo">Correo electrónico:</label>
+                    <input type="email" id="correo" name="correo" required><br><br>
+
+                    <label for="direccion">Dirección:</label>
+                    <input type="text" id="direccion" name="direccion" required><br><br>
+
+                    <input type="submit" value="Registrar">
+                </form>
+            </body>
+            </html>
+            """
+        return html
+
+
+# Función para leer la base de datos RDF
+def leer_DB(ruta_archivo):
+    base_datos = Graph()
+    try:
+        base_datos.parse(ruta_archivo, format="xml")
+    except Exception as e:
+        print("Error:", e)
+    return base_datos
+
+# Función para añadir un usuario a la base de datos RDF
+def registrarUsuarioBD(nombre, correo, direccion):
+    base_datos = leer_DB(archivo_base_datos_usuarios)
+
+    usuario_uri = ns["Usuario_" + str(uuid.uuid4())]  # Generar URI única para el usuario
+    base_datos.add((usuario_uri, RDF.type, ns.Usuario))
+    base_datos.add((usuario_uri, ns.Nombre, Literal(nombre)))
+    base_datos.add((usuario_uri, ns.Correo, Literal(correo)))
+    base_datos.add((usuario_uri, ns.Direccion, Literal(direccion)))
+
+    base_datos.serialize(destination=archivo_base_datos_usuarios, format="xml")
+
+
+@app.route('/buscar', methods=['GET', 'POST'])
+def buscar():
     if request.method == 'POST':
         nom = request.form['nom']
         preu_min = request.form['preu_min']
@@ -59,12 +131,12 @@ def home():
                     return render_template('productesFiltratsClient.html', productos=productosFiltrados)
                 else:
                     print("No se encontraron productos.")
-                    return "No se encontraron productos. <br><a href='/'>Volver</a>"
+                    return "No se encontraron productos. <br><a href='/buscar'>Volver</a>"
 
             else:
                 mensaje = "Productos no trobats." if response.status_code == 404 else "Error en el envío de los filtros."
                 return f"""{mensaje}<br>
-                <a href="/">Tornar</a>
+                <a href="/buscar">Tornar</a>
                 """
         except Exception as e:
             # Manejo de errores
@@ -156,13 +228,13 @@ def comprar():
         if not carritoCompra:
             return """
             El carrito está vacío. Añade productos antes de proceder a la compra.<br>
-            <a href="/">Volver a la página principal</a>
+            <a href="/buscar">Volver a la página principal</a>
             """
         try:
             datosCompra = {
                 "carrito": carritoCompra,
-                "usuario_id": USER_ID,
-                "direccion": direccion
+                "usuario_id": USUARIO_REGISTRADO.nombre,
+                "direccion": USUARIO_REGISTRADO.direccion
             }
             response = requests.post(
                 VENDEDOR_URL + '/ProcesarCompra', json=datosCompra)
@@ -170,11 +242,11 @@ def comprar():
                 carritoCompra.clear()
                 global precioTotal
                 precioTotal = 0
-                return "Compra realizada con éxito.<br><a href='/'>Volver a la página principal</a>"
+                return "Compra realizada con éxito.<br><a href='/buscar'>Volver a la página principal</a>"
             else:
-                return f"Error al procesar la compra: {response.text}<br><a href='/'>Volver a la página principal</a>"
+                return f"Error al procesar la compra: {response.text}<br><a href='/buscar'>Volver a la página principal</a>"
         except Exception as e:
-            return f"Error al enviar la información al vendedor: {e}<br><a href='/'>Volver a la página principal</a>"
+            return f"Error al enviar la información al vendedor: {e}<br><a href='/buscar'>Volver a la página principal</a>"
     else:
         return """
         <form method="post">
@@ -182,7 +254,7 @@ def comprar():
             <input type="text" id="direccion" name="direccion" required><br><br>
             <input type="submit" value="Comprar">
         </form>
-        <br><a href="/">Volver a la página principal</a>
+        <br><a href="/buscar">Volver a la página principal</a>
         """
 
 
